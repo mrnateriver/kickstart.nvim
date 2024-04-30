1aded42a01863a92b9e26e498a05c8963c4401ae
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2024-04-30 21:09:01 +0200

    chore: add a bunch of commented out themes just in case

--- init.lua
+++ init.lua
@@ -977,6 +977,70 @@ require('lazy').setup({
       vim.cmd.colorscheme 'tokyonight'
     end,
   },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --
+  --   lazy = false,
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       flavour = 'mocha',
+  --       transparent_background = true,
+  --     }
+  --
+  --     vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  -- },
+  -- {
+  --   'scottmckendry/cyberdream.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('cyberdream').setup {
+  --       -- Recommended - see "Configuring" below for more config options
+  --       transparent = true,
+  --       italic_comments = true,
+  --       hide_fillchars = true,
+  --       borderless_telescope = false,
+  --       terminal_colors = true,
+  --     }
+  --     vim.cmd 'colorscheme cyberdream' -- set the colorscheme
+  --   end,
+  -- },
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('kanagawa').setup {
+  --       compile = true, -- enable compiling the colorscheme
+  --       -- undercurl = true, -- enable undercurls
+  --       -- commentStyle = { italic = true },
+  --       -- functionStyle = {},
+  --       -- keywordStyle = { italic = true },
+  --       -- statementStyle = { bold = true },
+  --       -- typeStyle = {},
+  --       transparent = true, -- do not set background color
+  --       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --       terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --       -- colors = { -- add/modify theme and palette colors
+  --       --   palette = {},
+  --       --   theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --       -- },
+  --       overrides = function(colors) -- add/modify highlights
+  --         return {}
+  --       end,
+  --       theme = 'wave', -- Load "wave" theme when 'background' option is not set
+  --       -- background = { -- map the value of 'background' option to a theme
+  --       --   dark = 'wave', -- try "dragon" !
+  --       --   light = 'lotus',
+  --       -- },
+  --     }
+  --
+  --     vim.cmd.colorscheme 'kanagawa'
+  --   end,
+  -- },
 
   --[[ { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
